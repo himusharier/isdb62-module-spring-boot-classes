eab0fe2f7f25ee26773f903032c59cd83f7d8b26
Sharier Himu <himusharier@gmail.com>
2025-03-05 12:13:32 +0600

    data insert, delete done

---
+++ project-with-database/src/main/java/com/example/project_with_database/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.project_with_database.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project_with_database.model.Student;
+import com.example.project_with_database.service.StudentService;
+
+@RestController
+@RequestMapping(value = "/student")
+public class StudentController {
+	private final StudentService service;
+
+	public StudentController(StudentService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	public Student saveStudent(@RequestBody Student student) {
+		Student saveStudent = service.saveStudent(student);
+		return saveStudent;
+	}
+
+	@GetMapping
+	public List<Student> getStudents() {
+		return service.getStudents();
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable int id) {
+		service.deleteById(id);
+	}
+
+}
--- project-with-database/src/main/java/com/example/project_with_database/service/StudentService.java
+++ project-with-database/src/main/java/com/example/project_with_database/service/StudentService.java
@@ -1,5 +1,32 @@
 package com.example.project_with_database.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.project_with_database.model.Student;
+import com.example.project_with_database.repository.StudentRepository;
+
+@Service
 public class StudentService {
+
+	private final StudentRepository repository;
+
+	public StudentService(StudentRepository repository) {
+		this.repository = repository;
+	}
+
+	public Student saveStudent(Student student) {
+		Student saved = repository.save(student);
+		return saved;
+	}
+
+	public List<Student> getStudents() {
+		return repository.findAll();
+	}
+
+	public void deleteById(int id) {
+		repository.deleteById(id);
+	}
 
 }
