3423f06d8ce9c529d0d154d0448ded8c6105df8a
Sharier Himu <himusharier@gmail.com>
2025-03-20 17:56:58 +0600

    ecom-project initiated (backend + frontend)

---
+++ ecom-project/ecom-backend/src/test/java/com/himusharier/ecombackend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himusharier.ecombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
