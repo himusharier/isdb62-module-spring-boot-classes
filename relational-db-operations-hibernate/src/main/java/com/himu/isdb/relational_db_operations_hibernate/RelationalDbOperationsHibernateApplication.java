e24c4129d66a85213cce10f67af14f9665e5aee8
Sharier Himu <himusharier@gmail.com>
2025-03-22 13:04:12 +0600

    relation database design project added

---
+++ relational-db-operations-hibernate/src/main/java/com/himu/isdb/relational_db_operations_hibernate/RelationalDbOperationsHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.himu.isdb.relational_db_operations_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDbOperationsHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDbOperationsHibernateApplication.class, args);
+	}
+
+}
---
+++ relational-db-operations-hibernate/src/test/java/com/himu/isdb/relational_db_operations_hibernate/RelationalDbOperationsHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himu.isdb.relational_db_operations_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDbOperationsHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
