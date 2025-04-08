4fb35821d696483e1a7be3c270142f9e296a41fd
Sharier Himu <himusharier@gmail.com>
2025-04-09 02:08:24 +0600

    spring boot security by amigos (install security)

---
+++ spring-boot-security/src/main/java/com/workshop/himusharier/HimusharierApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.himusharier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimusharierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimusharierApplication.class, args);
+	}
+
+}
