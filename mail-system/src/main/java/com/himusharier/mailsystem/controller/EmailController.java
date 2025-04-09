7689463a61fd80f46ad06f09cbbc02667882e4ed
Sharier Himu <himusharier@gmail.com>
2025-04-09 14:30:08 +0600

    mailing service added

---
+++ mail-system/src/main/java/com/himusharier/mailsystem/controller/EmailController.java
@@ -0,0 +1,31 @@
+package com.himusharier.mailsystem.controller;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import com.himusharier.mailsystem.model.EmailRequest;
+import com.himusharier.mailsystem.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.mail.MessagingException;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+
+	@PostMapping("/send-email")
+	public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+		try {
+			emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+			return ResponseEntity.ok("Email sent successfully");
+		} catch (MessagingException | IOException | GeneralSecurityException e) {
+			return ResponseEntity.internalServerError().body("Failed to send email: " + e.getMessage());
+		}
+	}
+}
