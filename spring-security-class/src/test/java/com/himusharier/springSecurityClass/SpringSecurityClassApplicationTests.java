a24c110acc462124974bda739649922a391929d6
Sharier Himu <himusharier@gmail.com>
2025-04-10 13:01:33 +0600

    spring security project added

---
+++ spring-security-class/src/test/java/com/himusharier/springSecurityClass/SpringSecurityClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himusharier.springSecurityClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
