49bb7d2142842526d7dde0dc899b5319700b6354
Sharier Himu <himusharier@gmail.com>
2025-04-13 02:03:31 +0600

    security-yt, version downgrade and SecurityConfig file added

---
+++ spring-boot-security/src/main/java/com/workshop/himusharier/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.workshop.himusharier.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
